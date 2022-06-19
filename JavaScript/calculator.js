d4ba54a0eb893d7b762c78a9986defbbf75826b7
Asrani-Aman <96644946+Asrani-Aman@users.noreply.github.com>
2022-06-19 23:32:18 +0530

    first-commit

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -1,14 +1,16 @@
 /*document.write("jai shree ram");*/
 // Todo: Make M+ M- and MC functional
 let string = "";
+document.querySelector('input').value = '0';
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
       string = eval(string);
       document.querySelector('input').value = string;
+      string='';
     }
-    else if(e.target.innerHTML == 'C'){
+    else if(e.target.innerHTML == "AC"){
       string = ""
       document.querySelector('input').value = string;
     }
@@ -19,3 +21,14 @@ Array.from(buttons).forEach((button)=>{
       }
   })
 })
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src = "../dark theme icon/sun.png"
+  }
+  else{
+    icon.src = "../dark theme icon/moon.png"
+  }
+}
